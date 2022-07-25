da94deaa261391f6e12e80adcf8340709d5a20ad
Mr. EmPee <manuel.palumbo@email.com>
2022-07-25 15:13:38 +0200

    First Commit

---
+++ src/main/java/com/aizoon/rendicontazione/RendicontazioneApplication.java
@@ -0,0 +1,13 @@
+package com.aizoon.rendicontazione;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RendicontazioneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RendicontazioneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aizoon/rendicontazione/RendicontazioneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aizoon.rendicontazione;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RendicontazioneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
