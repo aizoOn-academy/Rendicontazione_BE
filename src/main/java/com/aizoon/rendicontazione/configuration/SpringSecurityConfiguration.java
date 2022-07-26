2e58b14265c5b82c3868c9719faab574ff7d70ab
Mr. EmPee <manuel.palumbo@email.com>
2022-07-26 14:29:55 +0200

    Removed XSRF vulnerability

--- src/main/java/com/aizoon/rendicontazione/configuration/SpringSecurityConfiguration.java
+++ src/main/java/com/aizoon/rendicontazione/configuration/SpringSecurityConfiguration.java
@@ -16,13 +16,8 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.security.web.authentication.HttpStatusEntryPoint;
+import org.springframework.security.web.authentication.logout.HttpStatusReturningLogoutSuccessHandler;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-
-import java.util.Arrays;
-import java.util.List;
 
 @Configuration
 @EnableWebSecurity
@@ -41,27 +36,22 @@ public class SpringSecurityConfiguration {
         }
 
         http.authorizeRequests()
-                .antMatchers("/login").permitAll()
+                .antMatchers("/api/login").permitAll()
                 .anyRequest().authenticated();
 
-        //Disabling default spring form login, we will use our own login form
+        //Adding logout capabilities
+        http.logout( customizer -> {
+            AntPathRequestMatcher logoutMatcher = new AntPathRequestMatcher("/api/logout", "POST");
+            
+            customizer.logoutRequestMatcher(logoutMatcher);
+            customizer.defaultLogoutSuccessHandlerFor(new HttpStatusReturningLogoutSuccessHandler(), logoutMatcher);
+        });
+
         //Replacing the default authentication filter (It was using parameters in the request to authenticate)
         http.addFilter(jsonAuthenticationFilter);
+
         http.exceptionHandling().authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.FORBIDDEN));
-
         return http.build();
-    }
-
-    @Bean
-    public CorsConfigurationSource corsConfigurationSource() {
-        CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:4200"));
-        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
-        configuration.setAllowedHeaders(Arrays.asList("authorization", "content-type"));
-        configuration.setAllowCredentials(true);
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", configuration);
-        return source;
     }
 
     @Bean
@@ -73,7 +63,7 @@ public class SpringSecurityConfiguration {
     public JsonAuthenticationFilter jsonAuthenticationFilter(AuthenticationManager authenticationManager, AuthenticationFailureHandler authenticationFailureHandler, AuthenticationSuccessHandler authenticationSuccessHandler) {
         JsonAuthenticationFilter filter = new JsonAuthenticationFilter();
         filter.setRequiresAuthenticationRequestMatcher(
-                new AntPathRequestMatcher("/login", "POST")
+                new AntPathRequestMatcher("/api/login", "POST")
         );
 
         filter.setAuthenticationManager(authenticationManager);
